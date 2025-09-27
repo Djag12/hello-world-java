3d684271442e0bcda03d76a730a757f29fe9c298
Destin Gilbert <your_email@example.com>
2025-09-26 20:35:37 -0400

    Assignment 1: Hello World program moved into assignment1 folder

---
+++ src/org/howard/edu/lsp/assignment1/Assignment1.java
@@ -0,0 +1,9 @@
+// Destin Gilbert
+package org.howard.edu.lsp.assignment1;
+
+public class Assignment1 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+
